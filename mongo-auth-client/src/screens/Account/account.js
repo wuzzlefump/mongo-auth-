ee91893eb7ea17f417f0cb25cabbf7e4441345b3
Paul Elliott <paulelliottwork@gmail.com>
2021-03-03 23:17:51 -0700

    capture login and signup values on the front end

--- mongo-auth-client/src/screens/Account/account.js
+++ mongo-auth-client/src/screens/Account/account.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Account = ()=>{
+const Account = (props)=>{
 
 
     return(<h1></h1>)
--- mongo-auth-client/src/screens/Login/login.js
+++ mongo-auth-client/src/screens/Login/login.js
@@ -1,15 +1,45 @@
 import React, {useState, useEffect} from 'react'
+import './style.css'
 
 const Login = (props)=>{
 
+  //toggle between signup and login
     const [loginState, setLoginState]=useState(true)
-    const toggleLogin = ()=>{
+      const toggleLogin = ()=>{
         if(loginState === true){
             setLoginState(false)
         }else{
             setLoginState(true)
         }
     }
+    //toggle
+    //signup states
+    const [signUpEmail, setSignUpEmail]=useState()
+    const [signUpPass, setSignUpPass]=useState()
+    //signup States
+    // Login States
+    const [loginEmail, setLoginEmail]=useState()
+    const [loginPass, setLoginPass]=useState()
+    // Login States
+
+    //handleLogin
+    const handleLogin = (e)=>{
+      e.preventDefault()
+      let request = {
+        email:loginEmail,
+        password:loginPass
+      }
+      console.log(request)
+    }
+    const handleSignup = (e)=>{
+      e.preventDefault()
+      let request = {
+        email:signUpEmail,
+        password:signUpPass
+      }
+      console.log(request)
+    }
+
 
     return(loginState === true ?
         <div className="loginCard">
@@ -26,6 +56,8 @@ const Login = (props)=>{
                 placeholder="user@example.com"
                 required
                 className="formInput"
+                value = {loginEmail}
+                onChange={(e)=>{setLoginEmail(e.target.value)} }
               />
             </div>
             <div className="formField">
@@ -38,14 +70,63 @@ const Login = (props)=>{
                 placeholder="enter your password"
                 required
                 className="formInput"
+                value={loginPass}
+                onChange={(e)=>{setLoginPass(e.target.value)} }
               />
             </div>
-            <button type="submit" className="submitButton">
+            <div className="loginButtonContainer">
+            <button type="submit" className="submitButton" onClick={handleLogin}>
               Sign in
             </button>
+            <button type="submit" className="submitButton" onClick={toggleLogin}>
+              New User
+            </button>
+            </div>
           </form>
         </div>
       </div> : 
-         <div></div>)
+         <div className="loginCard">
+           <div className="formHeader">Mongo Auth User Sign Up</div>
+        <div className="formContainer">
+          <form  className= "form">
+            <div className="formField">
+              <label htmlFor="email" className="formLabel">
+                Email
+              </label>
+              <input
+                type="text"
+                name="email"
+                placeholder="user@example.com"
+                required
+                className="formInput"
+                value={signUpEmail}
+                onChange={(e)=>{setSignUpEmail(e.target.value)}}
+              />
+            </div>
+            <div className="formField">
+              <label htmlFor="password" className="formLabel">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="enter your password"
+                required
+                className="formInput"
+                value={signUpPass}
+                onChange={(e)=>{setSignUpPass(e.target.value)}}
+              />
+            </div>
+            <div className="loginButtonContainer">
+            <button type="submit" className="submitButton" onClick={handleSignup}>
+              Sign up
+            </button>
+            <button type="submit" className="submitButton" onClick={toggleLogin}>
+              Existing User
+            </button>
+            </div>
+          </form>
+          </div>
+         </div>)
 }
 export default Login;
